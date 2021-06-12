0c35fca3c965fff49c10d0e28d0c03fd15f394e9
PatchWorkCoding <85509178+PatchWorkCoding@users.noreply.github.com>
2021-06-12 09:26:14 -0500

    Implemented Ore Deposit

---
+++ GMTKJam2021-Unity/Assets/Source/Code/Runtime/OreDeposit.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreDeposit : MonoBehaviour
+{
+    [SerializeField]
+    OreTypes oreState = OreTypes.METAL;
+    [SerializeField]
+    int depositAmount = 20;
+
+    public OreTypes Type
+    {
+        get { return oreState; }
+    }
+
+    public int Amount
+    {
+        get { return depositAmount; }
+        set { depositAmount = value; }
+    } 
+}
+
+public enum OreTypes
+{
+    OXYGEN, METAL
+}
