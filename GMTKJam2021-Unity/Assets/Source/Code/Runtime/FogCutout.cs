ed1441aa2e15a15c7fb6c9e2dc955def91ff955e
Zachary Sheppard <the_ds_guy@yahoo.com>
2021-06-12 22:30:14 -0500

    Fixed a bug with fog cutouts so they no longer live on the units themselves

---
+++ GMTKJam2021-Unity/Assets/Source/Code/Runtime/FogCutout.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityAtoms.BaseAtoms;
+using UnityEngine;
+
+public class FogCutout : MonoBehaviour
+{
+
+    #region Public Properties
+
+    public float CutoutScale => m_cutoutScale;
+
+    #endregion
+
+    #region Private Methods
+
+    private void OnEnable()
+    {
+        StartCoroutine(OnEnable_Routine());
+    }
+
+    private IEnumerator OnEnable_Routine()
+    {
+        while (FogOfWar.Instance == null)
+        {
+            yield return null;
+        }
+
+        FogOfWar.Instance.RegisterCutout(this);
+    }
+
+    private void OnDisable()
+    {
+        if (FogOfWar.Instance)
+        {
+            FogOfWar.Instance.UnregisterCutout(this);
+        }
+    }
+
+    #endregion
+
+    #region Private Fields
+
+    [SerializeField]
+    private FloatReference m_cutoutScale = new FloatReference(1f);
+
+    #endregion
+
+}
